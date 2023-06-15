a99d06a41a70c24b9034352402a512cc18f6f9f3
maanil17 <mlaad17@gmail.com>
2023-06-16 02:59:01 +0530

    portfolio site

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#0a192f] text-gray-300 flex flex-col justify-center items-center pt-2'>
+        <a href="https://twitter.com/mlaad17">Twitter</a>
+        <a href="https://github.com/maanil17">Github</a>
+        
+    </div>
+    
+  )
+}
+
+export default Footer
---
+++ src/data/data.js
@@ -0,0 +1,43 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import Flixpedia from '../assets/projects/flixpedia.png'
+import chatter from '../assets/projects/chatter.png'
+import harry from '../assets/projects/harrypotter.png'
+import weather from '../assets/projects/weather.png'
+
+export const data=[
+    {
+        id:1,
+        
+        image:Flixpedia,
+        github:"https://github.com/maanil17/Flixpedia",
+        live:"https://flixpedia.vercel.app/",
+        bio:"Explore detailed TV shows and movies info, enjoy a user-friendly interface and smooth browsing.",
+    },
+    {
+        id:2,
+       
+        image:chatter,
+        github:"https://github.com/maanil17/react-firebase-chat",
+        live:"https://chat-174d4.web.app/",
+        bio:"A private group chat app made with react and firebase. Users can form rooms and chat with multiple users at same time!",
+    },
+    {
+        id:3,
+      
+        image:harry,
+        github:"https://github.com/maanil17/harrry-potter-wiki",
+        live:"https://harrypotter-info-mlaad17.netlify.app/",
+        bio:"Made Harry Potter Charcters info app with React JS. You can search any character's name and get its details. Also you can filter characters on the basis of their house and gender.",
+    },
+    {
+        id:4,
+       
+        image:weather,
+        github:"https://github.com/maanil17/weather-app",
+        live:"https://maanil17.github.io/weather-app/",
+        bio:"Weather app made using Weatherbit.io API data.",
+    },
+
+
+]
